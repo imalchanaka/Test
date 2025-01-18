26b9cb639965ef16c747d6a7e2a376fadf183e59
imal chanaka <107134405+imalchanaka@users.noreply.github.com>
2025-01-19 02:26:02 +0530

    initiate first comiit

---
+++ first.js
@@ -0,0 +1,42 @@
+// let no1=150;
+// let no2='150';
+
+// if(no1===no2)
+// {
+//     alert(true)
+// }
+// else{
+//     alert(false)
+// }
+
+// const Pi=Math.PI;
+
+
+
+//console.log(Pi);
+// let day=2;
+
+// switch(day){
+
+//     case 1:
+//         console.log('Mondy');
+//         break;
+//     case 2:
+//         console.log('Thuseday')
+
+// }
+
+let mobile=["nokia","Samsung","apple"]
+
+for (let value of mobile){
+
+    console.log(value);
+}
+
+let name={firstname:"imal",LastName:"chanaka"}
+
+for (let key in name){
+
+
+    console.log(key);
+}
